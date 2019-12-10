f7a184b8e1bcb729b1b28efa0c36eb19f6b25137
ZashyAris <sharonatieno080@gmail.com>
2019-12-10 10:36:53 +0300

    Added a function to the js file it will toggle the design part of the website

--- js/script.js
+++ js/script.js
@@ -1,29 +1,46 @@
 $(document).ready(function(){
-    $("#designimg , #design").click(function(){
-        $ ("#design").toggle()
-        $ ("#designimg").toggle()
+    $("#designImg").click(function(){
+        $("#designImg").hide();
+        $("#design").toggle();
+        $("#design").show();
     });
-$(document).ready(function(){
-    $('.card-dev p').hide();
-    $('.card-dev').click(function(){
-        $('.card-dev p').toggle();
-        $('.card-dev img').toggle();
-    });
-});
-$(document).ready(function(){
-    $('.bottom p').hide();
-    $('.bottom').click(function(){
-        $('.bottom p').toggle();
-        $('.bottom img').toggle();
-    });
-    $('.col').hover(function(){
-      $('.text',this).toggle('slow');
-    });
-    $('.submit').click(function(){
-       var name= $('#name').val();
-       var email=$('#email').val();
-       var message=$('#message').val();
-       if ((name!=="") &&(email!=="") &&(message!==""));
-       alert(name  +"," +" Thank you.Your  form has been submitted successfully");
+    $("#design").click(function(){
+        $("#design").hide();
+        $("#designImg").toggle();
+        $("#designImg").show();
     });
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

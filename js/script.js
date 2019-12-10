ea52a4849fad1da62324fe9c96e30fd2423aa3af
ZashyAris <sharonatieno080@gmail.com>
2019-12-10 11:24:30 +0300

    Added a function which will act as the hover effect

--- js/script.js
+++ js/script.js
@@ -1,74 +1,50 @@
-$(document).ready(function(){
-    $("#designImg").click(function(){
-        $("#designImg").hide();
-        $("#design").toggle();
-        $("#design").show();
-    });
-    $("#design").click(function(){
-        $("#design").hide();
-        $("#designImg").toggle();
-        $("#designImg").show();
-    });
+$(document).ready(function() {
+  $("#designImg").click(function() {
+    $("#designImg").hide();
+    $("#design").toggle();
+    $("#design").show();
+  });
+  $("#design").click(function() {
+    $("#design").hide();
+    $("#designImg").toggle();
+    $("#designImg").show();
+  });
 });
-$(document).ready(function(){
-    $("#devimg").click(function(){
-        $("#devimg").hide();
-        $("#development").toggle();
-        $("#development").show();
-    });
-    $("#development").click(function(){
-        $("#development").hide();
-        $("#devimg").toggle();
-        $("#devimg").show();
-    });
+$(document).ready(function() {
+  $("#devimg").click(function() {
+    $("#devimg").hide();
+    $("#development").toggle();
+    $("#development").show();
+  });
+  $("#development").click(function() {
+    $("#development").hide();
+    $("#devimg").toggle();
+    $("#devimg").show();
+  });
 });
-$(document).ready(function(){
-    $("#productimg").click(function(){
-        $("#productimg").hide();
-        $("#product").toggle();
-        $("#product").show();
-    });
-    $("#product").click(function(){
-        $("#product").hide();
-        $("#productimg").toggle();
-        $("#productimg").show();
-    });
-    $("#subscribe").click(function(){
-        var user = document.getElementById("name").value;
-       alert("Thank you "+user+" for subscribing to our channel");
-    });
+$(document).ready(function() {
+  $("#productimg").click(function() {
+    $("#productimg").hide();
+    $("#product").toggle();
+    $("#product").show();
+  });
+  $("#product").click(function() {
+    $("#product").hide();
+    $("#productimg").toggle();
+    $("#productimg").show();
+  });
+  $("#subscribe").click(function() {
+    var user = document.getElementById("name").value;
+    alert("Thank you " + user + " for subscribing to our channel");
+  });
+
+  // Add hover effect
+  $(".potfolio img").hover(
+    function() {
+      $(this).fadeTo(300, 0.6);
+    },
+    function() {
+      $(this).fadeTo(300, 1);
+    }
+  );
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-

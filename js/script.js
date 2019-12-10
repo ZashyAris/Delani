fc71fbdf9270f7427aea0ba59403e4301e46095f
ZashyAris <sharonatieno080@gmail.com>
2019-12-10 10:41:20 +0300

    Added  another function in the js file it will toggle the production part of the website

--- js/script.js
+++ js/script.js
@@ -22,6 +22,22 @@ $(document).ready(function(){
         $("#devimg").show();
     });
 });
+$(document).ready(function(){
+    $("#productimg").click(function(){
+        $("#productimg").hide();
+        $("#product").toggle();
+        $("#product").show();
+    });
+    $("#product").click(function(){
+        $("#product").hide();
+        $("#productimg").toggle();
+        $("#productimg").show();
+    });
+    $("#subscribe").click(function(){
+        var user = document.getElementById("name").value;
+       alert("Thank you "+user+" for subscribing to our channel");
+    });
+});
 
 
 
